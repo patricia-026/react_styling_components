8e806292f3f5c1aa5b8554f2de9a51e27cc03cd9
Patricia <kovach.patri@gmail.com>
2023-11-11 11:15:15 +0100

    dynamic & conditional inline styles

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -27,6 +27,9 @@ export default function AuthInputs() {
           <label>Email</label>
           <input
             type="email"
+            // style={{
+            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+            // }}
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
