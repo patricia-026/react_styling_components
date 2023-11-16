cfe5f57bfa0103ae719fb7ce21d1daa87111194c
Patricia <kovach.patri@gmail.com>
2023-11-16 14:13:42 +0100

    dynamic & conditional styling with tailwind

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,23 @@
-import { styled } from "styled-components";
+export default function Input({ label, invalid, ...props }) {
+  let labelClasses = 'block mb-2 text-xs font-bold tracking-wide uppercase';
+  let inputClasses = 'w-full px-3 py-2 leading-tight border rounded shadow';
+
+  if (invalid) {
+    labelClasses += ' text-red-400';
+    inputClasses += ' text-red-500 bg-red-100 border-red-300';
+  }
+  else {
+    labelClasses += ' text-stone-300';
+    inputClasses += ' text-gray-700 bg-stone-300';
+  }
+
+  return <p>
+    <label className={labelClasses}>{label}</label>
+    <input className={inputClasses} {...props} />
+  </p>
+}
+
+/*import { styled } from "styled-components";
 
 const Label = styled.label`
   display: block;
@@ -26,4 +45,4 @@ export default function CustomInput({ label, invalid, ...props }) {
         <Label $invalid={invalid}>{label}</Label>
         <Input $invalid={invalid} {...props} />
     </p>
-}
+}*/
